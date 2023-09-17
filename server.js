aaca9c564e9ecd2ce27437fb33eef8dae1517d41
avinash2906 <72144928+avinash2906@users.noreply.github.com>
2023-09-17 17:59:21 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,33 +1,31 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static("public"));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 375
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
-
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
+// app.get('/budget', (req, res) => {
+//     res.json(budget);
+// })
+
 app.get('/budget', (req, res) => {
-    res.json(budget);
-})
+    fs.readFile('budgetData.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to read budget data' });
+        return;
+      }
+      // Parse the JSON data
+      const budget = JSON.parse(data);
+      // Send the budget data as a JSON response
+      res.json(budget);
+    });
+  });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
